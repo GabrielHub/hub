20009bbbbb82b59f49516c89684cc853a26dc9d7
Gabriel Ong <ryer168@gmail.com>
2023-03-13 18:40:30 -0700

    setup dependencies and router

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ErrorBoundary } from 'components/ErrorBoundary';
+import { Root } from 'routes/root';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: '/hub',
+    element: <Root />,
+    errorElement: <ErrorBoundary />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
