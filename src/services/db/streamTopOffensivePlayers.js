5fcb7594824d70d77e7766399654f4251bb2a43a
Gabriel Ong <ryer168@gmail.com>
2023-04-11 01:28:10 -0700

    remove extra logs

--- src/services/db/streamTopOffensivePlayers.js
+++ src/services/db/streamTopOffensivePlayers.js
@@ -3,8 +3,6 @@ import { db } from 'services/firestore';
 
 export const streamTopOffensivePlayers = (sortField, sortType, snapshot, error) => {
   const collectionRef = collection(db, 'players');
-  console.log('sortField', sortField);
-  console.log('sortType', sortType);
   const queryRef = query(collectionRef, orderBy(sortField, sortType), limit(10));
   return onSnapshot(queryRef, snapshot, error);
 };
